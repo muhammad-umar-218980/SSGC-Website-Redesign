6968a1f18b3b9a44caefee4a9ce8296fd96fe38a
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-07-14 09:43:58 +0500

    Added all components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 // src/App.tsx
 import HeroSection from './components/HeroSection';
+// import QuickLinks from './components/QuickLinks';
+import ViewBill from './components/Viewbill';
+import CompanyNews from './components/CompanyNews';
+import AdMapPartners from './components/AdMapPartners';
+import FocalPersons from './components/FocalPersons';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <HeroSection />
+      {/* <QuickLinks /> */}
+      <ViewBill />
+      <CompanyNews />
+      <AdMapPartners />
+      <FocalPersons />
+      <Footer/>
     </div>
   );
 }
