a7c2aeedcc39f726a0ad07db1c679b3600fc77dc
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-07-17 10:13:55 +0500

    Integrated all components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-// src/App.tsx
-import HeroSection from './components/HeroSection';
-// import QuickLinks from './components/QuickLinks';
-import ViewBill from './components/Viewbill';
-import CompanyNews from './components/CompanyNews';
-import AdMapPartners from './components/AdMapPartners';
-import FocalPersons from './components/FocalPersons';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CoreValuesPage from "./pages/CoreValuesPage";
+import FlameMagazinePage from "./pages/FlameMagazinePage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <HeroSection />
-      {/* <QuickLinks /> */}
-      <ViewBill />
-      <CompanyNews />
-      <AdMapPartners />
-      <FocalPersons />
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/corevalues" element={<CoreValuesPage />} />
+        <Route path="/flamemagazine" element={<FlameMagazinePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
